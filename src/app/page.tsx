9c37dbe63abfed30e621f7b279df176a3ca848d1
Krishna Mohan <krishnamohan@192.168.1.16>
2024-12-29 17:25:36 +0530

    added shadcn and init the projcct

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+
+  return (
+    <main className="text-red-400">
+      hello world
+      <Button>Hello </Button>
+    </main>
+  );
+}
