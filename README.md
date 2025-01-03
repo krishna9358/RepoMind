28e443c83a6e912d4beb16cfc0048e80af185fa9
Krishna Mohan <krishanmohank974@gmail.com>
2025-01-04 01:49:32 +0530

    created trpc protected route

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Authentication : clerk
 project bundle : t3
 package manager : bun 
 ui : shadcn
-tech-stack : nextjs, typescript, tailwindcss, prisma, postgresql
+tech-stack : nextjs, typescript, tailwindcss, prisma, postgresql,trpc
+
+
+Need to brush upon : Trpc
--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,18 +1,20 @@
 "use client"
-import { FormInput } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { FormInput, Plus } from "lucide-react";
 import Image from "next/image";
 import { useForm } from "react-hook-form";
 
 type FormInput = {
-    repoUrl : string
-    projectName : string
-    githubToken? : string
+    repoUrl: string
+    projectName: string
+    githubToken?: string
 }
 
-export default function CreatePage(){
-    const {register, handleSubmit, reset} = useForm<FormInput>()
-    function onSubmit(data:FormInput){
-        window.alert(data)
+export default function CreatePage() {
+    const { register, handleSubmit, reset } = useForm<FormInput>()
+    function onSubmit(data: FormInput) {
+        window.alert(JSON.stringify(data,null,2))
         return true;
     }
     return (
@@ -22,10 +24,17 @@ export default function CreatePage(){
                 <div>
                     <h1 className="font-semibold text-2xl">Link your GitHub Repository</h1>
                     <p className="text-sm text-muted-foreground">Enter the URL of your Repository to link it to RepoMind</p>
-                </div>  
+                </div>
                 <div className="h-4"></div>
                 <div>
                     <form onSubmit={handleSubmit(onSubmit)}>
+                        <Input type="text" required {...register('projectName', { required: true })} placeholder="Project Name" />
+                        <div className="h-2"></div>
+                        <Input type="text" required {...register('repoUrl', { required: true })} placeholder="Github Repository URL" />
+                        <div className="h-2"></div>
+                        <Input type="text"  {...register('githubToken')} placeholder="Github Token" />
+                        <div className="h-2"></div>
+                        <Button type="submit" className="btn btn-primary"> <Plus /> Create Project</Button>
 
                     </form>
                 </div>
